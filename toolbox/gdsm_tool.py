5a5b1ea58dde8f96a3ae316dd99c6b33cd38281d
Allan Oware <lumtegis@gmail.com>
2018-07-12 19:51:59 +0300

    added param file

--- toolbox/gdsm_tool.py
+++ toolbox/gdsm_tool.py
@@ -5,7 +5,7 @@
 #	arcpy
 #
 # Global Soil Data Manager Toolbox
-# Tested on ArcGIS 10.5 and
+# Tested on ArcGIS 10.5
 #
 
 import os, sys, subprocess
@@ -18,10 +18,20 @@ area_of_interest = arcpy.GetParameterAsText(2)
 
 r_script_path = "D:\\2018\CIAT\src\\"
 
-def run_gsdm(soil_sample_data, raster_map, area_of_interest):
+def create_param_file(soil_sample_data, raster_map, area_of_interest):
+    # write parameters to text file
+    param_file = r_script_path + "params.txt"
+    file = open(param_file, "w")
+    file.write(soil_sample_data+"\n")
+    file.write(raster_map+"\n")
+    file.write(area_of_interest+"\n")
+    file.close()
+
+def run_gsdm():
     # takes user specified data, pass to configuration file and run R script
     r_cmd = "Rscript " + r_script_path + "example_modified.R > log.txt"
     subprocess.call(r_cmd, shell=False)
 
 if __name__ == '__main__':
-    run_gsdm(soil_sample_data, raster_map, area_of_interest)
+    create_param_file(soil_sample_data, raster_map,area_of_interest)
+    run_gsdm()

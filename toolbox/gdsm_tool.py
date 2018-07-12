083dd096ac5abf9a09db958d3731d4595c4ad67e
Allan Oware <lumtegis@gmail.com>
2018-07-12 18:25:30 +0300

    deactivated shell

--- toolbox/gdsm_tool.py
+++ toolbox/gdsm_tool.py
@@ -21,7 +21,7 @@ r_script_path = "D:\\2018\CIAT\src\\"
 def run_gsdm(soil_sample_data, raster_map, area_of_interest):
     # takes user specified data, pass to configuration file and run R script
     r_cmd = "Rscript " + r_script_path + "example_modified.R > log.txt"
-    subprocess.call(r_cmd, shell=True)
+    subprocess.call(r_cmd, shell=False)
 
 if __name__ == '__main__':
     run_gsdm(soil_sample_data, raster_map, area_of_interest)
